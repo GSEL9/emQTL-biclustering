cac4f2f31bd7f0ca596e7dfa7f1c30623a15033e
SeverinLangberg <Langberg91@gmail.com>
2018-08-04 20:33:09 +0200

    WIP: CCS algorithm

--- clustering.py
+++ clustering.py
@@ -2,6 +2,8 @@
 
 # TODO: Merge with data-science tools.
 # TODO: Specify version rpy2 >= 2.2.2 in dependencies.
+# TODO: Include verbose to R algs to supress warnings
+# TODO: Include BiMax
 
 """
 Various scikit-learn compatible clustering algorithms.
@@ -23,6 +25,25 @@ from rpy2.rinterface import RRuntimeError
 
 
 rpy2.robjects.numpy2ri.activate()
+
+
+
+"""Utility functions for dealing with R"""
+#from rpy2 import interactive as r
+#import rpy2.robjects as robjects
+
+#try:
+#    from rpy2.robjects.packages import importr
+#except ImportError:
+#    from rpy2.interactive import importr
+#base = importr('base')
+#def get_bioclite():
+#    """
+#    Note: requires an internet connection.
+#
+#    """
+#    base.source("http://bioconductor.org/biocLite.R")
+#    return robjects.r['biocLite']
 
 
 class BiclusterList(list):
@@ -348,7 +369,7 @@ class ChengChurch(RBiclusterBase):
 
         return self.transform(X, y=y, **kwargs)
 
-
+# ERROR: Passing fit.model produces error `$ operator is invalid for atomic vectors`
 class Plaid(RBiclusterBase):
     """
 
@@ -364,7 +385,7 @@ class Plaid(RBiclusterBase):
 
     params = {
         'cluster': 'b',
-        #'fit.model': 'y ~ m + a + b',
+        'fit.model': robjects.r('y ~ m + a + b'),
         'background': True,
         'row.release': 0.7,
         'col.release': 0.7,
@@ -500,36 +521,148 @@ class XMotifs(RBiclusterBase):
         return self.transform(X, y=y, **kwargs)
 
 
+"""Binary utility functions"""
+
+import os
+import shutil
+import tempfile
+import subprocess
+
+
+class PathError(Exception):
+    """Error raised if binary algorithm executable is not located on $PATH."""
+
+    def __init__(self, message):
+
+        super().__init__(message)
+
+
+# TODO: Replace with check_binary which raises error instead of returning None?
+def path_to_program(program, path_var='PATH'):
+    """Check if an executable is included in $PATH environment variable."""
+
+    def is_exec(fpath):
+
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exec(program):
+            return program
+        #else:
+        #    return None
+    else:
+        for path in os.environ[path_var].split(os.pathsep):
+
+            exe_file = os.path.join(path, program)
+            if is_exec(exe_file):
+                return exe_file
+            #else:
+            #    return None
+
+    return None
+
+
 class BinaryBiclusteringBase:
+    """
 
-    def make_temp_dir():
-
-        pass
-
-    def write_data():
-
-        pass
-
-    def hold_results():
-
-        pass
-
-    def read_back_results():
-
-        pass
-
-    def cleanup_temp_dirs():
-
-        pass
-
-
-class CBP(BinaryBiclusteringBase):
-    """A wrapper for the CPB binary.
-
-    Detects biclusters based on row correlations.
+    Attribtues:
+        binary ():
+        temp_dir ():
 
     """
 
+    def __init__(self, binary=None, file_format='txt', temp=False):
+
+        self.binary = binary
+        self.file_format = file_format
+        self.temp = temp
+
+        # NOTE: Variables set with instance.
+        self.path_dir = None
+        self.path_data = None
+
+    @property
+    def binary(self):
+
+        return self._binary
+
+    @binary.setter
+    def binary(self, value):
+
+        if path_to_program(value) is None:
+            raise PathError('Executable {0} not on $PATH'.format(value))
+        else:
+            self._binary = value
+
+    @property
+    def path_dir(self):
+
+        return self._path_dir
+
+    @path_dir.setter
+    def path_dir(self, value):
+
+        self._path_dir = value
+
+    @property
+    def path_data(self):
+
+        return self._path_data
+
+    @path_data.setter
+    def path_data(self, value):
+
+        if value is None:
+            return
+        else:
+            if isinstance(value, str):
+                self._path_data = value
+            else:
+                raise ValueError('file path should be <str>, not {}'
+                                 ''.format(type(value)))
+
+    def io_setup(self):
+        # Set paths to temp dir holding the input and output data.
+
+        if self.temp:
+            self.path_dir = tempfile.mkdtemp()
+        else:
+            self.path_dir = '{0}_data'.format(self.binary)
+
+            if not os.path.exists(self.path_dir):
+                os.makedirs(self.path_dir)
+
+        self.path_data = os.path.join(
+            self.path_dir, 'data.{}'.format(self.file_format)
+        )
+
+    def io_teardown(self):
+
+        # Cleanup temporary directory
+        shutil.rmtree(self.path_dir)
+
+
+class CPB(BinaryBiclusteringBase):
+    """A wrapper for the CPB binary algorithm.
+
+    Detects biclusters based on row correlations.
+
+    Attribtues:
+        biclusters (list):
+
+    """
+
+    # Name
+    MODEL = 'cpb'
+
+    # File format of binary output data
+    FILE_FORMAT = 'txt'
+
+    # Initial bicluster
+    INIT_BINARY = 'init_bicluster'
+
+    # Hyperparameters
     params = {
         'nclus': 2,
         'targetpcc': 0.9,
@@ -537,46 +670,320 @@ class CBP(BinaryBiclusteringBase):
         'fixed_col': -1,
         'fixw': 0,
         'min_seed_rows': 3,
-        'max_seed_rows': None
+        'max_seed_rows': None,
+        'targetpcc': 0.9,
+        'fixw': 0
     }
 
-    def __init__(self, **kwargs):
+    def __init__(self, temp=False, **kwargs):
+
+        super().__init__(self.MODEL, self.FILE_FORMAT, temp)
 
         # TODO: Iterate through kwargs and update self.params
 
-        pass
+    @property
+    def biclusters(self):
+
+        return self._biclusters
+
+    @biclusters.setter
+    def biclusters(self, value):
+
+        if value is None:
+            return
+        else:
+            if isinstance(value, list):
+                self._biclusters = value
+            else:
+                raise ValueError('biclusters should be <list>, not {}'
+                                 ''.format(type(value)))
 
     def fit(self, X, y=None, **kwargs):
+        """"""
 
-        nrows, ncols = np.shape(X)
+        #_X = check_array(X)
 
-        if self.parmas['max_seed_rows'] is None:
-            self.parmas['max_seed_rows'] = nrows
+        self.format_input(X)
+        self.exec_clustering()
 
     def transform(self, X, y=None, **kwargs):
 
-        pass
+        self.biclusters = self.collect_output(X)
+
+        if self.temp:
+            self.io_teardown()
+
+        return self.biclusters
 
     def fit_transform(self, X, y=None, **kwargs):
 
-        pass
+        self.fit(X)
+
+        return self.transform(X)
+
+    # NOTE: Replace wit hmy own `pandas` method? Produces same result?
+    def format_input(self, X):
+
+        # NOTE: Base method to set paths for temp dir and data.
+        self.io_setup()
+
+        self.params['nrows'], self.params['ncols'] = np.shape(X)
+        with open(self.path_data, 'w') as outfile:
+
+            outfile.write(
+                '{0} {1}'.format(self.params['nrows'], self.params['ncols'])
+            )
+            for row in data:
+                outfile.write('\n')
+                # TODO: Convert map row data to str instead of list comp?
+                outfile.write(' '.join([str(value) for value in row]))
+            outfile.write('\n')
+            outfile.close()
+
+        return self
+
+    def exec_clustering(self):
+
+        # Create file holding results
+        self._setup_exec()
+
+        # Change to current working dir
+        current_loc = os.getcwd()
+
+        _path_data = os.path.abspath(self.path_data)
+        try:
+            os.chdir(self.path_dir)
+            command = (
+                '{model} {outfile} {initfile} 1 {targetpcc} {fixw}'.format(
+                    model=self.MODEL, outfile=_path_data, **self.params
+                )
+            )
+            subprocess.check_call(command.split())
+
+        except OSError:
+            raise PathError('Executable {0} not on $PATH'.format(value))
+
+        finally:
+            os.chdir(current_loc)
+
+    def _setup_exec(self):
+
+        _path_dir = os.path.abspath(self.path_dir)
+        parent, _ = os.path.split(_path_dir)
+        self.params['initfile'] = os.path.join(
+            parent, 'initfile.{0}'.format(self.FILE_FORMAT)
+        )
+        self.params['init_binary'] = self.INIT_BINARY
+
+        # Create initial bicluster file
+        command = (
+            '{init_binary} '
+            '{nrows} {ncols} {nclus} '
+            '{min_seed_rows} {max_seed_rows} {fixed_row} {fixed_col} '
+            '{initfile}'.format(**self.params)
+        )
+        try:
+            subprocess.check_call(command.split())
+        except OSError:
+            raise PathError('Executable {0} not on $PATH'
+                            ''.format(self.params['init_binary']))
+
+        return self
+
+    def collect_output(self, X):
+
+        results_dir = os.path.abspath(self.path_dir)
+        dir_files = os.listdir(results_dir)
+
+        biclusters = []
+        for _file in dir_files:
+
+            _, stem = os.path.splitext(_file)
+            if stem == '.out':
+
+                output_file = os.path.join(results_dir, _file)
+                biclusters.append(self.format_output(output_file, X))
+
+        return biclusters
+
+    def format_output(self, outfile, X):
+        """Reads the bicluster in a single CPB output file."""
+        rows, cols = [], []
+        with open(outfile, 'r') as resfile:
+
+            target = rows
+            for line in resfile:
+                if line[0] == 'R':
+                    continue
+                elif line[0] == 'C':
+                    target = cols
+                    continue
+                else:
+                    target.append(int(line.split()[0]))
+
+            rows.sort(), cols.sort()
+
+        return Bicluster(rows, cols, data=X)
 
 
 class CCS(BinaryBiclusteringBase):
+    """
 
-    def __init__(self):
+    Args:
+        thresh (float): A correlation threshold in range [0, 1]. Defaults to
+            0.8.
 
-        pass
+    Attribtues:
+        biclusters (list):
 
-    def fit(self, X, y=None, **kwargs):
+    """
 
-        pass
+    # Name
+    MODEL = 'ccs'
+
+    # File format of binary output data
+    FILE_FORMAT = 'txt'
+
+    # The standard dimensions of the input file
+    FILE_DIMS = 'Genes/Conditions'
+
+    params = {
+        'thresh': 0.8,
+        'bases': 1000,
+        'overlap': 100.0,
+        'out_format': 0
+    }
+
+    """Parameters:
+
+    -m [1 - number of gene/rows in the data matrix]: Set the number of base gene that are to be considered for forming biclusters.
+        Default value is 1000 or maximum number of genes when that is less than 1000.
+
+    -g [0.0 - 100.0]: Minimum gene set overlap required for merging the overlapped biclusters.
+        Default value is 100.0 for 100% overlap.
+
+    -p [0/1]: Set the output format. Default is 0. 0 - Print output in 3 rows.
+
+    Call syntax
+
+        input_file = ./Results/Synthetic_data_results/Data/Data_Constant_100_1_bicluster.txt
+        output_file = ./Results/Output_standard.txt
+
+        ccs -t 0.9 -i input_file -o output_file -m 50 -p 1 -g 100.0
+
+        bases, overlap, output
+
+    """
+
+    def __init__(self, temp=False):
+
+        super().__init__(self.MODEL, self.FILE_FORMAT, temp)
+
+    @property
+    def biclusters(self):
+
+        return self._biclusters
+
+    @biclusters.setter
+    def biclusters(self, value):
+
+        if value is None:
+            return
+        else:
+            if isinstance(value, list):
+                self._biclusters = value
+            else:
+                raise ValueError('biclusters should be <list>, not {}'
+                                 ''.format(type(value)))
+
+    def fit(self, X, y=None, sep='\t', **kwargs):
+
+        # TODO: Check if cuda is available in setup
+
+        self.format_input(X, sep=sep, **kwargs)
+        self.exec_clustering()
 
     def transform(self, X, y=None, **kwargs):
 
-        pass
+        self.biclusters = self.collect_output(X)
+
+        if self.temp:
+            self.io_teardown()
+
+        return self.biclusters
 
     def fit_transform(self, X, y=None, **kwargs):
+
+        self.fit(X)
+
+        return self.transform(X)
+
+    def format_input(self, X, sep='\t', **kwargs):
+
+        # NOTE: Base method to set paths for temp dir and data.
+        self.io_setup()
+
+        try:
+            rows = kwargs['']
+        except:
+            rows = ['row{0}'.format(str(num)) for num in range(X.shape[0])]
+        try:
+            cols = kwargs['genes']
+        except:
+            cols = [
+                '{0}col{1}'.format(sep, str(num)) for num in range(X.shape[1])
+            ]
+        with open(self.path_data, 'w') as outfile:
+            outfile.write(self.FILE_DIMS)
+            outfile.write(('').join(cols))
+            outfile.write('\n')
+            for num, row in enumerate(X):
+                outfile.write('{0}{1}'.format(rows[num], sep))
+                outfile.write(
+                    ('').join(['{0}{1}'.format(sep, str(val)) for val in row])
+                )
+                outfile.write('\n')
+            outfile.close()
+
+        return self
+
+    def exec_clustering(self):
+
+        # Create file holding results
+        self._setup_exec()
+
+        # Change to current working dir
+        current_loc = os.getcwd()
+
+        try:
+            os.chdir(self.path_dir)
+            # ccs -t 0.9 -i input_file -o output_file -m 50 -p 1 -g 100.0
+            command = (
+                '{model} -t {thresh} -i {infile} -o {outfile} '
+                '-m {bases} -p {out_format} -g {overlap} {out_format}'
+                ''.format(model=self.MODEL, **self.params)
+            )
+            subprocess.check_call(command.split())
+
+        except OSError:
+            raise PathError('Executable {0} not on $PATH'.format(value))
+
+        finally:
+            os.chdir(current_loc)
+
+    def _setup_exec(self):
+
+        self.params['infile'] = os.path.abspath(self.path_data)
+        self.params['outfile'] = os.path.join(
+            os.path.abspath(self.path_dir),
+            '{}_results.{}'.format(self.MODEL, self.FILE_FORMAT)
+        )
+
+    def collect_output(self):
+
+        pass
+
+    def format_output(self):
 
         pass
 
