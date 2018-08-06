771a1333d6d4f06658f1ed247170d6ea6332ad55
SeverinLangberg <Langberg91@gmail.com>
2018-08-06 19:15:56 +0200

    Added note and skeleton to gen test data functions

---
+++ datasets.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# testsets.py
+#
+
+"""
+Synthetic test data generators.
+
+Test data is generated in resemblance to the experimental datasets in order to
+evaluate the performance of the applied biclustering algorithms.
+
+The goal is to assess the algorithms capability of restoring the original test
+clusters. The test clusters are generated in similar proportions that was found
+to be biologically relevant in the experimental dataset.
+"""
+
+__author__ = 'Severin E. R. Langberg'
+__email__ = 'Langberg91@gmail.no'
+
+
+def gen_test_pvalues(sparse=True):
+    # If sparse: similar to selected data, if dense: similar to original data.
+
+    pass
+
+
+def gen_test_cpp(sparse=True):
+    # If sparse: similar to selected data, if dense: similar to original data.
+
+    pass
