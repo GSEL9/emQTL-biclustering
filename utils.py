0dc7c4c9af72241d4bc00d6a4c411cd18356e7ac
SeverinLangberg <Langberg91@gmail.com>
2018-08-06 10:59:31 +0200

    added check param function for one-liner type checking

--- utils.py
+++ utils.py
@@ -11,6 +11,16 @@ __author__ = 'Severin E. R. Langberg'
 __email__ = 'Langberg91@gmail.no'
 
 
+def check_parameter(name, param, target_dtype):
+    """Perform parameter type checking."""
+
+    if isinstance(param, target_dtype):
+        return
+    else:
+        raise TypeError('Invalid type {} to parameter `{}`. Should be {}'
+                        ''.format(type(param), name, target_dtype))
+
+
 class PathError(Exception):
     """Error raised if binary algorithm executable is not located on $PATH."""
 
