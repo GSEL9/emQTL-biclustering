f687bbd6743bd0b2ab547d94a309abd355955044
SeverinLangberg <Langberg91@gmail.com>
2018-08-06 19:31:00 +0200

    added expl to observations of exp data

--- datasets.py
+++ datasets.py
@@ -12,6 +12,11 @@ evaluate the performance of the applied biclustering algorithms.
 The goal is to assess the algorithms capability of restoring the original test
 clusters. The test clusters are generated in similar proportions that was found
 to be biologically relevant in the experimental dataset.
+
+Through exploration, the complete experimental dataset appear to exhibit a
+checkerboard structure. The reduced version of the same data contains the
+same general structure, but with with 1e7 fewer datapoints. This results in a
+sparse and a dense version of each dataset.
 """
 
 __author__ = 'Severin E. R. Langberg'
@@ -28,3 +33,11 @@ def gen_test_cpp(sparse=True):
     # If sparse: similar to selected data, if dense: similar to original data.
 
     pass
+
+
+if __name__ == '__main__':
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Start of with a checkerboard and modify the data
