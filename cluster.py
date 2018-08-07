f6eb803ecad99d7373fff3f4d9a8794d2321acb6
SeverinLangberg <Langberg91@gmail.com>
2018-08-07 15:14:53 +0200

    ERROR: Moved CPB to temp due to error in collecting bicluster indices

--- cluster.py
+++ cluster.py
@@ -218,6 +218,20 @@ class XMotifs(RBiclusterBase):
 class Spectral:
     """A wrapper for the scikit-learn spectral biclustering algorithms.
 
+    Kwargs:
+        n_clusters (int or tuple): (n_row_clusters, n_column_clusters),
+        method (str, {bistochastic, log, scale}): Method of normalizing and
+            converting singular vectors into biclusters.,
+        n_components (int): Number of singular vectors to check.,
+        n_best=3,
+        svd_method=’randomized’,
+        n_svd_vecs=None,
+        mini_batch=False,
+        init=’k-means++’,
+        n_init=10,
+        n_jobs=1,
+        random_state=None
+
     Attributes:
         rows_ ():
         columns_ ():
@@ -265,259 +279,8 @@ class Spectral:
         return self.transform(X, y=y, **kwargs)
 
 
-class CPB(BinaryBiclusteringBase):
-    """A wrapper for the CPB binary algorithm.
-
-    Detects biclusters based on row correlations.
-
-    Attributes:
-        rows_ ():
-        columns_ ():
-        biclusters_():
-        row_labels_ ():
-        column_labels_ ():
-
-    """
-
-    # Initial bicluster
-    INIT_BINARY = 'init_bicluster'
-
-    # Stem of output files holding the biclustering results.
-    STEM_OUTPUT_FILE = '.out'
-
-    # Hyperparameters
-    params = {
-        'nclus': 2,
-        'targetpcc': 0.9,
-        'fixed_row': -1,
-        'fixed_col': -1,
-        'fixw': 0,
-        'min_seed_rows': 3,
-        'max_seed_rows': None,
-        'targetpcc': 0.9,
-        'fixw': 0
-    }
-
-    def __init__(self, model='cpb', file_format='txt', temp=False, **kwargs):
-
-        super().__init__(model, file_format, temp)
-
-        # Iterate through kwargs and update parameters.
-        for key in kwargs:
-            if key in self.params.keys():
-                self.params[key] = kwargs[key]
-
-    def fit(self, X, y=None, **kwargs):
-
-        #_X = check_array(X)
-
-        # Call to base method to set paths for temp dir and data.
-        self.setup_io()
-        # Creates file of input data according to algorithm requirements.
-        self.format_input(X)
-        # Call to application
-        self.exec_clustering()
-
-    def transform(self, X, y=None, **kwargs):
-
-        # TODO: Check is fitted
-
-        self.fetch_biclusters(X)
-
-        if self.temp:
-            self.io_teardown_temp()
-
-        return self.biclusters_
-
-    def fit_transform(self, X, y=None, **kwargs):
-
-        self.fit(X, y=y, **kwargs)
-
-        return self.transform(X, y=y, **kwargs)
-
-    def format_input(self, X):
-
-        self.params['nrows'], self.params['ncols'] = np.shape(X)
-        with open(self.path_data, 'w') as outfile:
-
-            outfile.write(
-                '{0} {1}'.format(self.params['nrows'], self.params['ncols'])
-            )
-            for row in X:
-                outfile.write('\n')
-                # TODO: Convert map row data to str instead of list comp?
-                outfile.write(' '.join([str(value) for value in row]))
-            outfile.write('\n')
-            outfile.close()
-
-        return self
-
-    def exec_clustering(self):
-
-        # Create a file to hold the results.
-        self.create_results_file()
-        # Change to current working dir.
-        current_loc = os.getcwd()
-        try:
-            os.chdir(self.path_dir)
-            command = (
-                '{model} {outfile} {initfile} 1 {targetpcc} {fixw}'.format(
-                    model=self.model,
-                    outfile=os.path.abspath(self.path_data),
-                    **self.params
-                )
-            )
-            subprocess.check_call(command.split())
-
-        except OSError:
-            raise PathError('CPB not on $PATH')
-
-        finally:
-            os.chdir(current_loc)
-
-        return self
-
-    def create_results_file(self):
-
-        self.params['initfile'] = os.path.join(
-            self.path_dir, 'initfile.{0}'.format(self.file_format)
-        )
-        self.params['init_binary'] = self.INIT_BINARY
-
-        # Create initial bicluster file
-        command = (
-            '{init_binary} '
-            '{nrows} {ncols} {nclus} '
-            '{min_seed_rows} {max_seed_rows} {fixed_row} {fixed_col} '
-            '{initfile}'.format(**self.params)
-        )
-        try:
-            subprocess.check_call(command.split())
-        except OSError:
-            raise PathError('Executable {0} not on $PATH'
-                            ''.format(self.params['init_binary']))
-
-        return self
-
-    def fetch_biclusters(self, X):
-        """
-
-        """
-
-        results_dir = os.path.abspath(self.path_dir)
-        dir_files = os.listdir(results_dir)
-
-        num_biclusters = 0
-        rows, cols = [], []
-        for _file in dir_files:
-
-            _, stem = os.path.splitext(_file)
-            if stem == self.STEM_OUTPUT_FILE:
-
-                output_file = os.path.join(results_dir, _file)
-                row_idx, col_idx = self.format_output(output_file, X)
-                rows.append(row_idx), cols.append(col_idx)
-
-                num_biclusters += 1
-
-        # Create arrays of False.
-        row_clusters = np.zeros((num_biclusters, X.shape[0]), dtype=bool)
-        col_clusters = np.zeros((num_biclusters, X.shape[1]), dtype=bool)
-
-        for num in range(num_biclusters):
-            row_clusters[num, :][rows[num]] = True
-            col_clusters[num, :][cols[num]] = True
-
-        self.rows_, self.columns_ = row_clusters, col_clusters
-        self.biclusters_ = (self.rows_, self.columns_)
-
-        return self
-
-    def format_output(self, filename, X):
-        """Reads the bicluster in a single CPB output file.
-
-        Args:
-            filename (str):
-            X (array-like):
-
-        Returns:
-            (tuple): The bicluster row and column indicators.
-
-        """
-
-        rows, cols = [], []
-        with open(filename, 'r') as outfile:
-
-            target = rows
-            for line in outfile:
-                if line[0] == 'R':
-                    continue
-                elif line[0] == 'C':
-                    target = cols
-                    continue
-                else:
-                    target.append(int(line.split()[0]))
-
-            rows.sort(), cols.sort()
-
-        return rows, cols
-
-
 if __name__ == '__main__':
 
-    import matplotlib.pyplot as plt
+    # QUESTION: How to visualize the clustering result?
 
-    from sklearn.datasets import make_checkerboard
-    from sklearn.datasets import samples_generator as sg
-
-    from sklearn.metrics import consensus_score
-
-    # Generate sklearn sample data.
-    # rows: array of shape (n_clusters, X.shape[0],)
-    # columns: array of shape (n_clusters, X.shape[1],)
-    target, rows, columns = make_checkerboard(
-        shape=(500, 300), n_clusters=(4, 3), noise=10, shuffle=False,
-        random_state=0
-    )
-    data, row_idx, col_idx = sg._shuffle(target, random_state=0)
-
-    # Use sklearn model as reference to output structure
-    #ref_model = SpectralBiclustering()
-    #ref_model.fit(data)
-    #rows, cols = ref_model.rows_, ref_model.columns_
-    #print(rows.shape, cols.shape)
-
-    """
-    model = Plaid()
-    model.fit_transform(data)
-    rows, cols = model.rows_, model.columns_
-    print(rows.shape, cols.shape)
-
-    model = XMotifs()
-    model.fit_transform(data)
-    rows, cols = model.rows_, model.columns_
-    print(rows.shape, cols.shape)
-    """
-
-    model = Spectral()
-    model.fit_transform(data)
-    print(model.row_labels_)
-
-    """new_params = {
-        'nclus': 5,
-        'targetpcc': 5,
-        'fixed_row': 3,
-        'fixed_col': 4,
-    }
-    model = CPB(**new_params)
-    biclusters = model.fit_transform(data)
-    score = consensus_score(
-            biclusters, (rows[:, row_idx], columns[:, col_idx])
-    )"""
-
-    # NB: Moved to temp.
-    # NOTE: Super slow. Writes nothing to outfile check. Check if needs to
-    # change data or error with write output method.
-    #model = CCS()
-    #model.fit(data)
-    #model.transform(data)
+    pass
