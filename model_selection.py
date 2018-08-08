3027574d6d6f3b1a9de735bb556f53bcf7e55669
SeverinLangberg <Langberg91@gmail.com>
2018-08-08 10:48:54 +0200

    splitted execute into execute and model sel method to allow model sel over all test data classes in one experiment

--- model_selection.py
+++ model_selection.py
@@ -37,47 +37,99 @@ class Experiment:
 
     """
 
-    def __init__(self, data, rows, cols, verbose=True, seed=None):
+    def __init__(self, models_and_params, verbose=True, seed=None):
 
-        self.data = check_array(data)
-        self.rows = check_array(rows)
-        self.cols = check_array(cols)
-
-        #self.models_and_params = models_and_params
+        self.models_and_params = models_and_params
         self.verbose = verbose
         self.seed = seed
 
+        # NOTE: A cross val split producing only train and no test split.
+        self.dummy_cv = [(slice(None), slice(None))]
+
         # NOTE: Attributes set with instance.
+        self.data = None
+        self.rows = None
+        self.cols = None
+        # The latest (in case of execute()) model selection grid search object 
         self.grid = None
-        self.scores = None
-        self.extimes = None
-        self.best_settings = None
         self.best_model = None
+        #
+        self.model_stats = None
 
-    def execute(self, models_and_params):
-        """Conducts a grid search experiments to determine the optimal
-        hyperparameter settings with respect to the test data.
+    @property
+    def gen_gs_report(self):
+        """Generates and returns a report with grid search results."""
 
-        args:
-            models_and_params (list of typles): Holds tuples of algorithm and
-                corresponding parameter grid.
-            
+        target_cols = ['split0_test_score', 'mean_fit_time', 'params']
+
+        # Collect unnecessary column labels.
+        droppings = []
+        for key in self.grid.cv_results_.keys():
+            if key not in target_cols:
+                droppings.append(key)
+        # Remove unnecessary columns.
+        for key in droppings:
+            self.grid.cv_results_.pop(key)
+        # Create a properly formatted pandas.DataFrame
+        report = pd.DataFrame(
+            self.grid.cv_results_, columns=['score', 'train_time', 'params']
+
+        )
+        report.index.name = 'param_grid_num'
+
+        return report
+
+    # NOTE: Passing data to execute() and not constructor allows running
+    # experiments with same models and params over different datasets.
+    def execute(self, data, indicators):
+        """Performs a model comparison experiment by applying """
+
+        rows, cols = indicators
+
+        if self.verbose:
+            print('Conducting experiment:')
+
+        self.model_stats = {}
+        for key in test_data.keys():
+
+            if self.verbose:
+                print('Training set: {}'.format(key))
+
+            self.model_stats[key] = {}
+            self.data, self.rows, self.cols = data[key], rows[key], cols[key]
+
+            self.model_selection()
+
+    def model_selection(self):
+        """Compare the performance of models with optimal hyperparemeter
+        settings obtained from a grid search.
+
         """
 
         train, self.row_idx, self.col_idx = sgen._shuffle(
             self.data, random_state=self.seed
         )
-
-        for model, param_grid in models_and_params:
+        # Completes a single run over the data with each model.
+        for model, param_grid in self.models_and_params:
 
             if self.verbose:
                 print('Testing model: {}'.format(model.__name__))
 
             self.grid = GridSearchCV(
                 SpectralBiclustering(random_state=self.seed),
-                param_grid, scoring=self.jaccard, cv=[(slice(None), slice(None))]
+                param_grid, scoring=self.jaccard, cv=self.dummy_cv,
+                return_train_score=True
             )
             self.grid.fit(train, y=None)
+
+            # Best estimator from the grid search (i.e. the model fitted with
+            # the optimal param combo) = self.grid.best_estimator_
+            #
+
+            # QUESTION: How to determine th ebest model for this data?
+
+            if self.verbose:
+                self.performance_report(model.__name__)
 
         return self
 
@@ -86,10 +138,8 @@ class Experiment:
         two sets of biclusters.
 
         Args:
-            target_coords (tuple): The target row and column bicluster
-                indicators.
-            pred_coords (tuple): The predicted biclsuters with row and column
-                indicators.
+            estimator ():
+            train: Ignored
 
         Returns:
             (float): The Jaccard coefficient value.
@@ -102,6 +152,16 @@ class Experiment:
         score = consensus_score(ypred, ytrue)
 
         return score
+
+    def performance_report(self, name):
+        """Prints a model performance report including training and test scores,
+        and the difference between the training and test scores."""
+
+        print('Model performance', '\n{}'.format('-' * 24))
+        print('Name: {}'.format(name))
+
+
+        print('Avg train time {}')
 
 
 if __name__ == '__main__':
@@ -123,19 +183,32 @@ if __name__ == '__main__':
         data_feats, sparse=True, shape=(500, 300), n_clusters=3, seed=SEED
     )
 
+    # NOTE: Grid Search
+    # * Do not want to optimize with respect to the number of clusters. Pass
+    #   the correct number of clusters to allow optimization of other params.
+    # * With only one CV split (test dataset), the avg params are actually the
+    #   results for the whole run (which goes into the gs report).
+
+    # NOTE: Experiment
+    # * One execution = one run for each class of test data with given models
+    #   and params.
+    # * When conducting experiments (in ipynb) with a generated test dataset,
+    #   run the same experiment with the same dataset again as second
+    #   parallell addressing variability between experiments.
+
     models_and_params = [
         (
             SpectralBiclustering,
-            {'n_clusters': [2, 4], 'method': ['log']}
+            {'n_clusters': [3], 'method': ['log', 'bistochastic']}
         ),
-        (
-            SpectralCoclustering,
-            {'n_clusters': [2, 4]}
-        )
+        #(
+        #    SpectralCoclustering,
+        #    {'n_clusters': [2, 4]}
+        #)
     ]
 
     data = test_data[data_feats.index[0]]
     rows, cols = test_rows[data_feats.index[0]], test_cols[data_feats.index[0]]
 
-    experiment = Experiment(data, rows, cols)
-    experiment.execute(models_and_params)
+    experiment = Experiment(models_and_params)
+    experiment.execute(test_data, (test_rows, test_cols))
