13e7afd88bdd6e161ddcd62572ed45c668bfb5cd
SeverinLangberg <Langberg91@gmail.com>
2018-08-08 09:01:33 +0200

    BM: Initial working version og biclustering grid search

--- model_selection.py
+++ model_selection.py
@@ -20,76 +20,75 @@ __email__ = 'Langberg91@gmail.no'
 import time
 
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.utils.validation import check_array
+from sklearn.metrics import consensus_score
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.datasets import samples_generator as sgen
 
 
 class Experiment:
     """Perform experiments by applying an algorithm to data and measure the
-    performance."""
+    performance.
 
-    def __init__(self, models_and_params, verbose=True, seed=None):
+    Args:
+        data (array-like):
+        rows (array-like):
+        cols (array-like):
 
-        self.models_and_params = models_and_params
+    """
+
+    def __init__(self, data, rows, cols, verbose=True, seed=None):
+
+        self.data = check_array(data)
+        self.rows = check_array(rows)
+        self.cols = check_array(cols)
+
+        #self.models_and_params = models_and_params
         self.verbose = verbose
         self.seed = seed
 
         # NOTE: Attributes set with instance.
+        self.grid = None
         self.scores = None
         self.extimes = None
         self.best_settings = None
         self.best_model = None
 
-    def execute(self, X, y=None, score='index', **kwargs):
+    def execute(self, models_and_params):
+        """Conducts a grid search experiments to determine the optimal
+        hyperparameter settings with respect to the test data.
 
-        print(X)
+        args:
+            models_and_params (list of typles): Holds tuples of algorithm and
+                corresponding parameter grid.
+            
+        """
 
-        for model, param_grid in self.models_and_params:
+        train, self.row_idx, self.col_idx = sgen._shuffle(
+            self.data, random_state=self.seed
+        )
+
+        for model, param_grid in models_and_params:
 
             if self.verbose:
                 print('Testing model: {}'.format(model.__name__))
 
-            # NOTE: Iterate through each dataset
-            grid = GridSearchCV(
-                model(random_state=self.seed), param_grid,
-                scoring=self.jaccard,
-                cv=[(slice(None), slice(None))]
+            self.grid = GridSearchCV(
+                SpectralBiclustering(random_state=self.seed),
+                param_grid, scoring=self.jaccard, cv=[(slice(None), slice(None))]
             )
-
-            # TODO: Make scoring funciton capable to deal with
-            # need to bicluster output from wrapper predict,
-            # and provide a target to the grid
-
-            # for data, row, col in X:
-            #     grid.fit(data, target)
-
-
+            self.grid.fit(train, y=None)
 
         return self
 
-
-    def run_test(self, estimator, dataset, score):
-
-        data, rows, cols = dataset
-
-        start = time.time()
-        row_idx, col_idx = estimator.fit_transform(data)
-        extime = time.time() - start
-
-        jaccard = self._score((rows, cols), (row_idx, col_idx))
-
-        if score == 'indesx':
-            return jaccard
-        elif score == 'time':
-            return extime
-
-    def jaccard(self, target_coords, pred_coords):
+    def jaccard(self, estimator, train):
         """Computes the Jaccard coefficient as a measure of similarity between
         two sets of biclusters.
 
         Args:
-            target_coords (tuple): The original row and column bicluster
+            target_coords (tuple): The target row and column bicluster
                 indicators.
-            pred_coords (tuple): The predicted row and column bicluster
+            pred_coords (tuple): The predicted biclsuters with row and column
                 indicators.
 
         Returns:
@@ -97,86 +96,46 @@ class Experiment:
 
         """
 
-        rows, cols = target_coords
-        row_idx, col_idx = pred_coords
+        ypred = estimator.biclusters_
+        ytrue = (self.rows[:, self.row_idx], self.cols[:, self.col_idx])
 
-        score = consensus_score(
-            self.biclusters_, (rows[:, row_idx], columns[:, col_idx])
-        )
+        score = consensus_score(ypred, ytrue)
 
         return score
 
-    def results(self):
-
-        pass
-
-    def reports(self, graphical=True):
-
-        pass
-
-
-class GridSearchWrapper:
-    """A wrapper to enable scikit-learn grid search with biclustering
-    algorithms."""
-
-    def __init__(self):
-
-        pass
-
-    def fit(self):
-
-        pass
-
-    def score(self):
-        pass
-
-
 
 if __name__ == '__main__':
-    # Goal: Determine the model and hyperparameter settings corresponding to
-    # the best score (time/J index) across datasets.
-    #
-    # Algorithm: Grid Search
-    # ------------------------
-    # winning model = None
-    # for each model
-    #     best_local_scores, best_settings = None, None
-    #     for each hparam setting
-    #         for each class of test data
-    #             score, extime = run_test()
-    #         avg_scores = mean(score)
-    #         if avg_scores > best_local_scores
-    #             best_settings = num_hparam_setting
-    #
 
     import numpy as np
     import pandas as pd
 
+    from sklearn.cluster import SpectralBiclustering
+    from sklearn.cluster import SpectralCoclustering
+
     from datasets import gen_test_sets
-    from cluster import Spectral
 
     SEED = 0
 
     data_feats = pd.read_csv(
         './../data/data_characteristics.csv', sep='\t', index_col=0
     )
-    test_data, rows, cols = gen_test_sets(
+    test_data, test_rows, test_cols = gen_test_sets(
         data_feats, sparse=True, shape=(500, 300), n_clusters=3, seed=SEED
     )
 
-    test_data_configs = {
-        'shape': [],
-        'n_clusters': []
-    }
-
     models_and_params = [
         (
-            Spectral,
+            SpectralBiclustering,
             {'n_clusters': [2, 4], 'method': ['log']}
+        ),
+        (
+            SpectralCoclustering,
+            {'n_clusters': [2, 4]}
         )
     ]
 
-    #X = test_data[data_feats.index[0]]
+    data = test_data[data_feats.index[0]]
+    rows, cols = test_rows[data_feats.index[0]], test_cols[data_feats.index[0]]
 
-    #experiment = Experiment(models_and_params)
-    #experiment.execute((X, rows[0], cols[0]))
+    experiment = Experiment(data, rows, cols)
+    experiment.execute(models_and_params)
