158ecb0182454d4f80c8ed48751f8d84d5d67919
SeverinLangberg <Langberg91@gmail.com>
2018-08-09 10:51:37 +0200

    added performance report to multi experiment logging winning model avg and std perfomranve

--- model_selection.py
+++ model_selection.py
@@ -46,11 +46,23 @@ class PerformanceTracker:
         self.models = models
 
         # NOTE: Attribute set with instance
-        self.model_stats = self._setup_model_stats()
+        self.model_scores = self._setup_model_score_stats()
+        self.winning_stats = self._setup_model_win_stats()
 
-    def _setup_model_stats(self):
-        # Returns an object that logs model performance for each class of test
-        # data.
+    def _setup_model_score_stats(self):
+        # Returns an object that logs winning model score for each class of
+        # test data.
+
+        _model_stats = {}
+        for test_class in self.test_classes:
+            _model_stats[test_class] = {
+                model: [] for model in self.models
+            }
+
+        return _model_stats
+
+    def _setup_model_win_stats(self):
+        # Returns an object that counts model wins for each class of test data.
 
         _model_stats = {}
         for test_class in self.test_classes:
@@ -65,16 +77,19 @@ class PerformanceTracker:
         voted optimal for a particular test data class."""
 
         for test_class in self.test_classes:
-            name, _ = results[test_class]
-            self.model_stats[test_class][name] += 1
+            name, _, score = results[test_class]
+            self.winning_stats[test_class][name] += 1
+            self.model_scores[test_class][name].append(score)
+
+        return self
 
     @property
-    def best_models(self):
+    def winner_models(self):
         """Determines the optimal model for each class of test data."""
 
         winners = {}
         for test_class in self.test_classes:
-            candidates = self.model_stats[test_class]
+            candidates = self.winning_stats[test_class]
             winner, _ = max(candidates.items(), key=operator.itemgetter(1))
             winners[test_class] = winner
 
@@ -105,43 +120,11 @@ class Experiment:
         self.dummy_cv = [(slice(None), slice(None))]
 
         # NOTE: Attributes set with instance.
-        self.grids = None
         self.results = None
 
         self._data = None
         self._rows = None
         self._cols = None
-
-    # ERROR: Must gen report with results from all four classes of test data
-    @property
-    def performance_report(self):
-        """Generates and returns a report with grid search results."""
-
-        for grid in self.grids:
-            pass
-
-        """# NOTE: Only one CV split => avg score = score from single run.
-        target_cols = ['split0_test_score', 'mean_fit_time', 'params']
-
-        # Collect unnecessary column labels.
-        droppings = []
-        for key in self.grid.cv_results_.keys():
-            if key not in target_cols:
-                droppings.append(key)
-
-        # Remove unnecessary columns.
-        for key in droppings:
-            self.grid.cv_results_.pop(key)
-
-        # Create a properly formatted pandas.DataFrame
-        report = pd.DataFrame(
-            self.grid.cv_results_, columns=['score', 'train_time', 'params']
-
-        )
-        report.index.name = 'param_grid_num'
-
-        return report"""
-
 
     def execute(self, data, indicators, target='score'):
         """Performs model comparison for each class of test data."""
@@ -160,28 +143,30 @@ class Experiment:
             self._data = data[key]
             self._rows, self._cols = rows[key], cols[key]
 
-            # Holds the winning model for each class of test data.
-            winning_model, best_score = self.compare_models(target=target)
-
-            self.results[key] = winning_model
+            # Winning model, hest hparams, best score
+            self.results[key] = self.compare_models(target=target)
 
             if self.verbose > 0:
-                name, _ = winning_model
-                print('Best model: {}\nScore: {}\n'.format(name, best_score))
+                name, _, score = self.results[key]
+                print('Best model: {}\nScore: {}\n'.format(name, score))
 
         return self
 
     def compare_models(self, target):
         """Compare model performance on target basis."""
 
+        # Evaluates model performance by score value.
         if target == 'score':
-            return self.score_selection()
+            return self.score_eval()
+        # Evaluates model performance by time complexity.
         elif target == 'time':
-            return self.time_selection()
+            return self.time_eval()
         else:
-            raise ValueError('')
+            raise ValueError('Invalid target: `{}`'.format(target))
 
-    def score_selection(self):
+        return self
+
+    def score_eval(self):
         """Compare the model performance with respect to a score metric."""
 
         _train, self.row_idx, self.col_idx = sgen._shuffle(
@@ -189,7 +174,7 @@ class Experiment:
         )
         _train_std = self.scaler.fit_transform(_train)
 
-        winning_model, best_score = None, -np.float('inf')
+        winning_model, best_params, best_score = None, None, -np.float('inf')
         for model, param_grid in self.models_and_params:
 
             # Determine the best hyperparameter combo for that model
@@ -206,12 +191,10 @@ class Experiment:
 
             if grid.best_score_ > best_score:
                 best_score = grid.best_score_
-                winning_model = (model.__name__, model(**grid.best_params_))
+                winner_name = model.__name__
+                winner_model = model(**grid.best_params_)
 
-            # Store grid to generate performance report.
-            self.grids.append(grid)
-
-        return winning_model, best_score
+        return (winner_name, winner_model, best_score)
 
     def jaccard(self, estimator, train=None):
         """Computes the Jaccard coefficient as a measure of similarity between
@@ -237,14 +220,14 @@ class Experiment:
     # NOTE:
     # * Not performing grid search, but only logging time of fitting
     #   model to data?
-    def time_selection(self):
+    def time_eval(self):
 
         pass
 
 
 class MultiExperiment(Experiment):
 
-    def __init__(self, models_and_params, nruns=2, verbose=1, random_state=1):
+    def __init__(self, models_and_params, nruns=1, verbose=1, random_state=1):
 
         super().__init__(models_and_params, verbose, random_state)
 
@@ -257,7 +240,37 @@ class MultiExperiment(Experiment):
     @property
     def best_models(self):
 
-        return self._tracker.best_models
+        return self.results
+
+    @property
+    def class_winners(self):
+
+        return self._tracker.winner_models
+
+    @property
+    def model_votes(self):
+
+        return self._tracker.winning_stats
+
+    @property
+    def performance_report(self):
+
+        score_stats, test_classes = [], []
+        for test_class, winner in self.class_winners.items():
+
+            win_scores = self._tracker.model_scores[test_class][winner]
+            score_stats.append(
+                (winner, np.mean(win_scores), np.std(win_scores))
+            )
+            test_classes.append(test_class)
+
+        _report = pd.DataFrame(
+            score_stats, columns=['model', 'avg score', 'std score'],
+            index=test_classes
+        )
+        _report.index.name = 'test_class'
+
+        return _report
 
     @property
     def model_labels(self):
@@ -266,37 +279,34 @@ class MultiExperiment(Experiment):
 
     def execute_all(self, dataset, test_classes, target='score'):
 
-        self._multi_results = {str(num): None for num in range(self.nruns)}
         self._tracker = PerformanceTracker(test_classes, self.model_labels)
-        for num in range(self.nruns):
+        for run_num in range(self.nruns):
 
             if self.verbose > 0:
-                print('Run number: {}'.format(num))
+                print('Experiment parallel number: {}'.format(run_num + 1))
 
             # Perform single experiment with dataset.
-            for num, (data, rows, cols) in enumerate(dataset):
+            for class_num, (data, rows, cols) in enumerate(dataset):
+
+                if self.verbose > 0:
+                    print('Test class number: {}'.format(class_num + 1))
 
                 # Perform model comparison with test data class.
                 self.execute(data, (rows, cols), target=target)
-                # NOTE: The counter is updated for each run. The winning models
-                # is thus the winners across all runs.
-                self._tracker.update_stats(self.results)
 
-            # Store the results for each experimental run.
-            self._multi_results[num] = self.results
+                # NOTE: Num wins counter is continued for each run.
+                self._tracker.update_stats(self.results)
 
         return self
 
 
 if __name__ == '__main__':
 
-    import datasets
+    import testsets
     import pandas as pd
 
     from sklearn.cluster import SpectralBiclustering
     from sklearn.cluster import SpectralCoclustering
-
-    from datasets import gen_test_sets
 
     SEED = 0
 
@@ -304,10 +314,10 @@ if __name__ == '__main__':
         './../data/data_characteristics.csv', sep='\t', index_col=0
     )
     array_size = (1000, 100)
-    var_num_clusters = [2, 3]
-
+    var_num_clusters = [2, 4, 8]
+    # NOTE: Each list element is a tuple of data, rows, cols
     cluster_exp_data = [
-        datasets.gen_test_sets(
+        testsets.gen_test_sets(
             data_feats, sparse=[False, True, False, True],
             non_neg=[False, True, False, True],
             shape=array_size, n_clusters=n_clusters, seed=0
@@ -316,16 +326,25 @@ if __name__ == '__main__':
     ]
 
     skmodels_and_params = [
-        (
-            SpectralBiclustering, {
-                'n_clusters': [5], 'method': ['log', 'bistochastic'],
-                'n_components': [6, 9, 12], 'n_best': [3, 6]
-            }
-        ),
-        (
-            SpectralCoclustering, {'n_clusters': [5]}
-        )
-    ]
+    (
+        SpectralBiclustering, {
+            'n_clusters': var_num_clusters, 'method': ['log', 'bistochastic'],
+            'n_components': [6, 9, 12], 'n_best': [3, 6]
+        }
+    ),
+    (
+        SpectralCoclustering, {'n_clusters': var_num_clusters}
+    )
+]
 
-    me = MultiExperiment(skmodels_and_params)
-    me.execute_all(cluster_exp_data, data_feats.index)
+    sk_multi_exper = MultiExperiment(skmodels_and_params, verbose=0)
+    sk_multi_exper.execute_all(cluster_exp_data, data_feats.index)
+    print(sk_multi_exper.best_models)
+    print(sk_multi_exper.class_winners)
+    print(sk_multi_exper.performance_report)
+    print(sk_multi_exper.model_votes)
+
+    # Collects:
+    # * Names and num wins of each model : num_model_wins
+    # * The winner models with hparams for each test class: best_models
+    # * The scores for each winner model: winner_stats
