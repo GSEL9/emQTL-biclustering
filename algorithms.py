26cf4015d204b04dfc30886611beb5f1bd0acc2e
SeverinLangberg <Langberg91@gmail.com>
2018-08-15 16:23:36 +0200

    implemented R biclust bimax wrapper

--- algorithms.py
+++ algorithms.py
@@ -16,6 +16,9 @@ import rpy2.robjects as robjects
 
 from base import RBiclusterBase
 
+# NOTE: To binarize Bimax input.
+from sklearn.preprocessing import binarize
+
 
 class ChengChurch(RBiclusterBase):
     """A wrapper for the R BCCC algorithm.
@@ -169,6 +172,110 @@ class Plaid(RBiclusterBase):
         return self
 
 
+class Bimax(RBiclusterBase):
+    """A wrapper for the R biclust package BCBimax algorithm.
+
+    Searches for submatrices of ones in a logical matrix.
+
+    """
+
+    MODEL = 'BCBimax'
+
+    # Hyperparameters
+    params = {
+        'minr': 4,
+        'minc': 4,
+        'number': 2,
+    }
+
+    def __init__(self, random_state=0, n_clusters=1, thresh=20, **kwargs):
+
+        super().__init__(random_state=random_state, **kwargs)
+
+        self.thresh = thresh
+
+        # Update parameters.
+        for key in kwargs:
+            if key in self.params.keys():
+                self.params[key] = kwargs[key]
+
+        self.set_params(**kwargs)
+
+        # NOTE:
+        self._output = None
+
+        self.rows_ = None
+        self.columns_ = None
+        self.biclusters_ = None
+
+    def fit(self, X, y=None, **kwargs):
+
+        X_binary = binarize(X, threshold=self.thresh)
+
+        self._fit(self.MODEL, X_binary, self.params)
+
+        return self
+
 if __name__ == '__main__':
 
-    pass
+    import numpy as np
+    import pandas as pd
+
+    from sklearn.datasets import make_biclusters
+    from sklearn.datasets import samples_generator as sg
+    from sklearn.metrics import consensus_score
+
+    """
+    data, rows, columns = make_biclusters(
+        shape=(300, 30), n_clusters=5, noise=5,
+        shuffle=False, random_state=0
+    )
+    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
+
+
+    model = Bimax()
+    model.fit(data)
+
+    score = consensus_score(model.biclusters_,
+                        (rows[:, row_idx], columns[:, col_idx]))
+
+    print("consensus score: {:.3f}".format(score))
+
+    """
+
+    import testsets
+    import model_selection
+
+    data_feats = pd.read_csv(
+        './../data/data_ids/data_characteristics.csv',
+        sep='\t', index_col=0
+    )
+
+    array_size = (1000, 100)
+    var_num_clusters = [2, 4, 6]
+
+    # NOTE: Each list element is a tuple of data, rows, cols
+    cluster_exp_data = [
+        testsets.gen_test_sets(
+            data_feats, sparse=[False, True, False, True],
+            non_neg=[False, True, False, True],
+            shape=array_size, n_clusters=n_clusters, seed=0
+        )
+        for n_clusters in var_num_clusters
+    ]
+
+    models_and_params = [
+    (
+        Bimax, {
+            'minc': [10, 20, 30],
+            'minr': [10, 20, 30]
+        }
+    )
+    ]
+
+    sk_multi_exper = model_selection.MultiExperiment(
+        models_and_params, verbose=0, random_states=[0, 1]
+    )
+    sk_multi_exper.execute_all(
+        cluster_exp_data, data_feats.index
+    )
